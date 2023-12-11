cc2de8512c25c8bb27d592200ae869f014ce828f
bnasare <124492150+bnasare@users.noreply.github.com>
2023-12-11 00:08:02 +0000

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
     return (
         <div>
             <Header />
             <Footer />
-            <Note />
+            {notes.map((noteItem) => (
+                <Note
+                    key={noteItem.key}
+                    title={noteItem.title}
+                    content={noteItem.content}
+                />
+            ))}
         </div>
     );
 }
